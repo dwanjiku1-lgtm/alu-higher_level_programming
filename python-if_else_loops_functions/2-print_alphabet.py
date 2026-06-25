b89e1b130dbb470628fd5bb761744097b898b409
dwanjiku1-lgtm <d.wanjiku1@alustudent.com>
2026-06-25 09:46:54 -0700

    task 2 mod 1

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for code in range(97, 123):
-    if code != 101 and code != 113:
-        print("{}".format(chr(code)), end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
