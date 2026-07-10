81fb0a0cd013975fd76b358cf389c4bbd5f14f97
dwanjiku1-lgtm <d.wanjiku1@alustudent.com>
2026-07-10 07:40:18 -0700

    task 3 mod

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,18 +2,18 @@
 def safe_print_division(a, b):
     # start with no result
     result = None
- 
+
     try:
         # try to divide a by b
         result = a / b
- 
+
     except ZeroDivisionError:
         # if b is 0, we can't divide, so result stays None
         result = None
- 
+
     finally:
         # always print the result, whether it worked or not
         print("Inside result: {}".format(result))
- 
+
     # return the result
     return result
