651f4d5ac012b1f925ed212c1d25289b1b7d6f2e
dwanjiku1-lgtm <d.wanjiku1@alustudent.com>
2026-07-30 12:29:40 -0700

    task 5

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""A simple Rectangle class."""
+
+
+class Rectangle:
+    """This class makes a rectangle shape."""
+
+    def __init__(self, width=0, height=0):
+        """Make a new rectangle with a width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Give back the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Change the width, but check it first."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Give back the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Change the height, but check it first."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Work out the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Work out the perimeter of the rectangle.
+        If width or height is 0, the perimeter is just 0.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Draw the rectangle using # symbols.
+        If width or height is 0, show nothing.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        lines = []
+        for i in range(self.__height):
+            lines.append("#" * self.__width)
+        return "\n".join(lines)
+
+    def __repr__(self):
+        """Give back text that can rebuild this same rectangle."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Say bye when the rectangle gets deleted."""
+        print("Bye rectangle...")
