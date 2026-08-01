6b1c072b6c7849e2d0d5a4d136931b3b4274b6c0
dwanjiku1-lgtm <d.wanjiku1@alustudent.com>
2026-08-01 06:10:36 +0200

    task mod 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Module that defines an is_same_class function."""
 
-
 def is_same_class(obj, a_class):
     """Check if obj is exactly an instance of a_class (no subclasses)."""
     return type(obj) == a_class
